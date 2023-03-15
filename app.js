ca852edf9dad6767c331363d9715048571f93cc2
Hasan Ali Balcioglu <h.alibalcioglu@gmail.com>
2023-03-15 21:53:34 +0300

    vercel fix

--- app.js
+++ app.js
@@ -5,24 +5,20 @@ const postRouter = require("./routes/post")
 const categoryRouter= require("./routes/category")
 const answerRouter = require("./routes/answer")
 app.use(express.json())
+const router = express.Router();
 
+router.get('/', function(req,res){
+    res.send({
+        version: 1
+    })
+});
+app.use(router)
 app.use("/question",postRouter)
 app.use("/category",categoryRouter)
 app.use("/answer",answerRouter)
+
 config()
-
-
-
-
 
 app.listen(process.env.PORT,()=>{
     console.log("Çalıştım!")
 })
-
-
-
-
-
-
-
-
